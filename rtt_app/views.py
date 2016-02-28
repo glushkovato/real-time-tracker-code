fd3177b108ae01b8f1657ae5a3aad6b23e8dbdbc
glushkovato <glushkovato@gmail.com>
2016-02-28 23:37:44 +0300

    Updated views

--- rtt_app/views.py
+++ rtt_app/views.py
@@ -1,11 +1,66 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post  # Поскольку views.py и models.py находятся в одной директории, мы можем использовать точку . и имя файла (без расширения .py)
+#from django.views.generic import TemplateView
+from .forms import PostForm
 
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'index.html'
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'rtt_app/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'rtt_app/post_detail.html', {'post': post})
 
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('rtt_app.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'rtt_app/post_edit.html', {'form': form})
 
+@login_required
+def post_edit(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('rtt_app.views.post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'rtt_app/post_edit.html', {'form': form})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'rtt_app/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('rtt_app.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('rtt_app.views.post_list')
+
+#class IndexView(TemplateView):
+    #template_name = 'index.html'
