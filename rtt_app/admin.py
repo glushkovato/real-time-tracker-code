901ca0baba4c7e3ee111abe5cb95badf94eb0efc
glushkovato <glushkovato@gmail.com>
2016-03-28 00:52:52 +0300

    Registered new models: Card and Task.

--- rtt_app/admin.py
+++ rtt_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from rtt_app.models import Post
+from rtt_app.models import Board, Task, Card
 
-admin.site.register(Post)
-
-
+admin.site.register(Board)
+admin.site.register(Task)
+admin.site.register(Card)
 # Register your models here.
