3ef0cc24d2db8a36ad01fccebb7b468bc8d54e29
glushkovato <glushkovato@gmail.com>
2016-03-28 00:55:41 +0300

    Updated urlpatterns.

--- rtt_app/urls.py
+++ rtt_app/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-    url(r'^post/(?P<pk>[0-9]+)/publish/$', views.post_publish, name='post_publish'),
-    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^$', views.board_list, name='board_list'),
+    url(r'^board/(?P<pk>[0-9]+)/$', views.board_detail, name='board_detail'),
+    url(r'^board/new/$', views.board_new, name='board_new'),
+    url(r'^board/(?P<pk>[0-9]+)/edit/$', views.board_edit, name='board_edit'),
+    url(r'^board/(?P<pk>[0-9]+)/publish/$', views.board_publish, name='board_publish'),
+    url(r'^board/(?P<pk>[0-9]+)/remove/$', views.board_remove, name='board_remove'),
+    url(r'^board/(?P<pk>[0-9]+)/card/new/$', views.card_create, name='card_create'),
+    url(r'^board/(?P<pk>[0-9]+)/card/(?P<card_id>[0-9]+)/remove/$', views.card_remove, name='card_remove'),
 ]
