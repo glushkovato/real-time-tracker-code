fd84b9603e4f2148b67a283ff216b6d51c6e39c9
glushkovato <glushkovato@gmail.com>
2016-06-18 00:12:22 +0300

    Added docstrings to BoardForm and CardForm.

--- rtt_app/forms.py
+++ rtt_app/forms.py
@@ -1,16 +1,24 @@
 from django import forms
-from .models import Board, Card, Task
+from .models import Board, Card
 
 
 class BoardForm(forms.ModelForm):
+    """
+    Note:
+        A form for creating a board.
 
+    """
     class Meta:
         model = Board
         fields = ('title',)
 
 
 class CardForm(forms.ModelForm):
+    """
+    Note:
+        A form for creating a card.
 
+    """
     class Meta:
         model = Card
         fields = ('title',)
