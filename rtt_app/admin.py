294d6ee71951c185e09e6e5fb3288cf2647a91a2
glushkovato <glushkovato@gmail.com>
2016-06-22 23:20:18 +0300

    Registered Task.

--- rtt_app/admin.py
+++ rtt_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from rtt_app.models import Board, Task, Card
+from rtt_app.models import Board, Card, Task
 
 admin.site.register(Board)
-admin.site.register(Task)
 admin.site.register(Card)
+admin.site.register(Task)
 # Register your models here.
